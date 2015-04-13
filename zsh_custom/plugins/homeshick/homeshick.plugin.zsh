299a91fc29445f60b15fd255bda78168b89e33cd
Chris Scutcher <chris.scutcher@ninebysix.co.uk>
2015-04-13 14:08:40 +0100

    Updated homeshick plugin for completion

--- zsh_custom/plugins/homeshick/homeshick.plugin.zsh
+++ zsh_custom/plugins/homeshick/homeshick.plugin.zsh
@@ -1,2 +1,12 @@
-source "$HOMESICK_REPOS/homeshick/homeshick.sh"
+HOMESHICK_REPO="$HOMESICK_REPOS/homeshick/"
+
+# Add functions
+source "$HOMESHICK_REPO/homeshick.sh"
+
+# Enable completion
+fpath=($HOMESHICK_REPO/completions $fpath)
+compinit
+
+# Refresh local
 homeshick --quiet --batch refresh | log_prefix_stdout
+
